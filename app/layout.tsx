f25ba708513ee3837b83da77280956041c26e483
Faysal Mridha <fmridha166@gmail.com>
2026-05-13 12:01:05 +0600

    feat: add Google Sans, Sora, and Urbanist fonts to layout for improved typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,29 @@
 import type { Metadata } from "next";
+import { Google_Sans, Sora, Urbanist } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "BIO Cleaning LLC",
   description: "Eco-friendly residential and commercial cleaning services.",
 };
+
+const google_sans = Google_Sans({
+  variable: "--font-google-sans",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+const sora = Sora({
+  variable: "--font-sora",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+const urbanist = Urbanist({
+  variable: "--font-urbanist",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +32,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`min-h-full flex flex-col  ${sora.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
